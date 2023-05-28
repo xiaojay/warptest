e58d2853cb88e4215f5025a981664d33a8f8e0c4
xiaojay <xiaojay@gmail.com>
2023-05-28 16:39:05 +0800

    add get state script

---
+++ src/state.ts
@@ -0,0 +1,15 @@
+import {WarpFactory} from 'warp-contracts';
+const contractId = '-8A6RexFkpfWwuyVO98wzSFZh0d6VJuI-buTJvlwOJQ';
+const warp = WarpFactory.forMainnet()
+const c = warp.contract(contractId)
+async function getState() {
+  try {
+    const { sortKey, cachedValue } = await c.readState();
+
+    console.log(sortKey, cachedValue.errorMessages, cachedValue.state, cachedValue.validity)
+  } catch (error) {
+    console.log('readState error:', error, 'contractId:', contractId);
+  }
+}
+
+getState()
